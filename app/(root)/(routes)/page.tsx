381571dc4462cdf6bc9dbbc8e898199cee67c3b3
karkiamrit <amrit.karki2073@gmail.com>
2023-10-28 14:32:29 +0545

    prisma and planetscale setup

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { UserButton } from "@clerk/nextjs";
- 
+import SearchInput from "@/components/search-input";
+import React from "react";
+
 const RootPage = () => {
   return (
-    <div >
-        <UserButton afterSignOutUrl="/" />
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
+    
     </div>
-  )
-}
+  );
+};
 
-export default RootPage
+export default RootPage;
